241e072f77fdc61bb536e90e45d8dac6d38ffb90
eminezubeyde <113181394+eminezubeyde@users.noreply.github.com>
2023-05-16 22:01:32 +0300

    project updated

--- microservice/inventory-service/src/main/java/com/kodlamaio/inventoryservice/business/abstracts/CarService.java
+++ microservice/inventory-service/src/main/java/com/kodlamaio/inventoryservice/business/abstracts/CarService.java
@@ -1,5 +1,6 @@
 package com.kodlamaio.inventoryservice.business.abstracts;
 
+import com.kodlamaio.commonpackage.utils.dto.ClientResponse;
 import com.kodlamaio.inventoryservice.business.dto.requests.create.CreateCarRequest;
 import com.kodlamaio.inventoryservice.business.dto.requests.update.UpdateCarRequest;
 import com.kodlamaio.inventoryservice.business.dto.responses.create.CreateCarResponse;
@@ -13,10 +14,16 @@ import java.util.UUID;
 
 public interface CarService {
     List<GetAllCarsResponse> getAll();
+
     GetCarResponse getById(UUID id);
+
     CreateCarResponse add(CreateCarRequest request);
+
     UpdateCarResponse update(UUID id, UpdateCarRequest request);
+
     void delete(UUID id);
-    void checkIfCarAvailable(UUID id);
+
+    ClientResponse checkIfCarAvailable(UUID id);
+
     void changeStateByCarId(State state, UUID id);
 }
