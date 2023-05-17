6ebc71b5d16ecc8984e18655eea9a3c086cf753e
eminezubeyde <113181394+eminezubeyde@users.noreply.github.com>
2023-05-17 13:07:38 +0300

    CarClientFallback Retry Workshop

--- microservice/rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/CarClientFallback.java
+++ microservice/rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/CarClientFallback.java
@@ -1,17 +1,19 @@
 package com.kodlamaio.rentalservice.api.clients;
 
 import com.kodlamaio.commonpackage.utils.dto.ClientResponse;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
-@Slf4j
+
 @Component
-public class CarClientFallback implements CarClient {
+public class CarClientFallback implements CarClient{
+
     @Override
+    @Retry(name = "retry-rental-client")
     public ClientResponse checkIfCarAvailable(UUID carId) {
-        log.info("INVENTORY SERVICE IS DOWN!");
         throw new RuntimeException("INVENTORY SERVICE IS DOWN!");
     }
 }
