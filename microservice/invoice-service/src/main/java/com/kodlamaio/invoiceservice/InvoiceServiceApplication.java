e8496b810b10acf2901b08f2a35f06d670633af7
eminezubeyde <113181394+eminezubeyde@users.noreply.github.com>
2023-05-27 18:53:20 +0300

    invoice service added

--- microservice/invoice-service/src/main/java/com/kodlamaio/invoiceservice/InvoiceServiceApplication.java
+++ microservice/invoice-service/src/main/java/com/kodlamaio/invoiceservice/InvoiceServiceApplication.java
@@ -1,9 +1,12 @@
 package com.kodlamaio.invoiceservice;
 
+import com.kodlamaio.commonpackage.utils.constants.Paths;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {Paths.ConfigurationBasePackage, Paths.Filter.ServiceBasePackage})
 public class InvoiceServiceApplication {
 
 	public static void main(String[] args) {
